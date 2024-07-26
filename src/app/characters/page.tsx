a10976b77e2c6033bc05a53458951f3c012138e8
MDamirchi <mdamirchi27@gmail.com>
2024-07-26 17:04:30 +0330

    update page.tsx

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { revalidateTag } from "next/cache";
 import CharacterCard from "@/components/CharacterCard";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import CharacterCardSkeleton from "@/components/CharacterCardSkeleton";
 
 interface character {
   image: string;
@@ -12,14 +8,10 @@ interface character {
   gender: string;
 }
 
-const test = [...Array(10)].fill(1);
-console.log(test);
-
 const getData = async () => {
   const res = await fetch("https://rickandmortyapi.com/api/character", {
     cache: "no-store",
   });
-  console.log({ res });
   const data = await res.json();
   return data.results;
 };
@@ -29,16 +21,15 @@ const Characters = async () => {
 
   return (
     <div className="w-full max-w-[1440px] mx-auto mt-10 grid grid-cols-3 gap-4">
-      {chars.length
-        ? chars.map((char: character) => (
-            <CharacterCard
-              image={char.image}
-              name={char.name}
-              status={char.status}
-              gender={char.gender}
-            />
-          ))
-        : test.map((ts) => <CharacterCardSkeleton />)}
+      {chars.map((char: character, index: number) => (
+        <CharacterCard
+          key={index}
+          image={char.image}
+          name={char.name}
+          status={char.status}
+          gender={char.gender}
+        />
+      ))}
     </div>
   );
 };
